23eb876b24ab66ad09974d3b53ea6039e49006e5
ThomasBrets <thomas15501331@gmail.com>
2024-06-28 23:50:55 -0300

    Start ProfileInfo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import ProfileInfo from "../components/Cards/ProfileInfo"
 
 const Navbar = () => {
   return (
     <div className="bg-white drop-shadow px-6 py-2 flex items-center justify-between">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+
+      <ProfileInfo/>
     </div>
   );
 };
