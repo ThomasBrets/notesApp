e74cb963e1993d538f1bde3b0e778e0866ebb403
ThomasBrets <thomas15501331@gmail.com>
2024-07-04 21:38:15 -0300

    start home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import Navbar from "../../components/Navbar"
+import React from "react";
+import Navbar from "../../components/Navbar";
+import NoteCards from "../../components/Cards/NoteCards";
 
 const Home = () => {
   return (
     <>
-    <Navbar />
-    
+      <Navbar />
+      <div className="container mx-auto">
+        <NoteCards
+          title="Inglés el 8 de julio"
+          date="2 de Julio"
+          content="Inglés el 8 de julio"
+          tags="#Meeting"
+          isPinned={true}
+          onEdit={() => {}}
+          onDelete={() => {}}
+          onPinNote={() => {}}
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
