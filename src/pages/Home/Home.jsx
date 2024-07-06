68e2039169b1c8fb20b0cc7d9812f2c5c97a545c
ThomasBrets <thomas15501331@gmail.com>
2024-07-06 18:52:22 -0300

    start modal

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import NoteCards from "../../components/Cards/NoteCards";
 import { MdAdd } from "react-icons/md";
 import AddEditNotes from "./AddEditNotes";
+import Modal from "react-modal";
+import { IoMdClose } from "react-icons/io";
 
 const Home = () => {
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
+
   return (
     <>
       <Navbar />
@@ -23,16 +31,30 @@ const Home = () => {
         </div>
       </div>
 
-      
-
       <button
         className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
-        onClick={() => {}}
+        onClick={() => {
+          setOpenAddEditModal({ isShown: true, type: "add", data: null });
+        }}
       >
         <MdAdd className="text-[32px] text-white" />
       </button>
 
-      <AddEditNotes />
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: "rgb(0,0,0,0.2)",
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-w-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
+        <IoMdClose className="absolute right-7 top-7 text-[20px] cursor-pointer text-slate-400 hover:text-slate-500 transition-all delay-200" onClick={() => setOpenAddEditModal({isShown:false})}/>
+
+        <AddEditNotes />
+      </Modal>
     </>
   );
 };
