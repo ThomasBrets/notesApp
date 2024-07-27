74a272671032e75d869105084e753ea18d5e335a
ThomasBrets <thomas15501331@gmail.com>
2024-07-27 17:39:38 -0300

    api front and create cluster mongodb

--- api/app.js
+++ api/app.js
@@ -1,1 +1,21 @@
-const express = require('express');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// Middlewares
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+    res.json({data:"hello"})
+})
+
+app.listen(3000, () => {
+
+})
+
+module.exports = app
