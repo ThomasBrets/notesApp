0e19a560c1853f4dd031cac83341dcc025fea08d
ThomasBrets <thomas15501331@gmail.com>
2024-09-30 19:49:27 -0300

    editNote works

--- api/routes/notes.js
+++ api/routes/notes.js
@@ -5,6 +5,9 @@ const NotesController = require("../controllers/notes")
 //! POST
 router.post("/add-note", NotesController.addNote)
 
+//! PUT
+router.put("/edit-note/:noteId", NotesController.editNote)
+
 
 
 module.exports = router
